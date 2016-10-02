5b45065eb0bcb2011e5da62c4f8f6b5c3ae33050
cgelinas1 <cgelinas@my.centennialcollege.ca>
2016-10-02 01:14:13 -0400

    ink and enemy spawn
    added ink shooting and enemy spawn

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    // Our enemy to spawn
+    public Transform enemy;
+
+    [Header("Wave Properties")]
+  
+    public float timeBeforeSpawning = 1.8f;
+    public float timeBetweenEnemies = .30f;
+    public float timeBeforeWaves = 2.0f;
+    public int enemiesPerWave = 4;
+    private int currentNumberOfEnemies = 0;
+
+    
+    private int score = 0;
+    private int waveNumber = 0;
+    private int lives = 5;
+
+   
+    void Start()
+    {
+        StartCoroutine(SpawnEnemies());
+    }
+
+   
+    IEnumerator SpawnEnemies()
+    {
+       
+        yield return new WaitForSeconds(timeBeforeSpawning);
+
+        // After timeBeforeSpawning has elapsed, we will enter this loop
+        while (true)
+        {
+            // Don't spawn until previous enemies are dead
+            if (currentNumberOfEnemies <= 0)
+            {
+                waveNumber++;
+                
+
+                //Spawn 10 enemies in a random position
+                for (int i = 0; i < enemiesPerWave; i++)
+                {
+                    // enemies off screen
+                    float randDistance = Random.Range(10, 25);
+
+                    // Enemies can come from any direction
+                    Vector2 randDirection = Random.insideUnitCircle;
+                    Vector3 enemyPos = this.transform.position;
+
+                    
+                    enemyPos.x += randDirection.x * randDistance;
+                    enemyPos.y += randDirection.y * randDistance;
+
+                    
+                    Instantiate(enemy, enemyPos, this.transform.rotation);
+                    currentNumberOfEnemies++;
+                    yield return new WaitForSeconds(timeBetweenEnemies);
+                }
+            }
+            // time before checking for new spawn
+            yield return new WaitForSeconds(timeBeforeWaves);
+        }
+    }
+
+    // Allows outside class to know when enemy killed
+    public void KilledEnemy()
+    {
+        currentNumberOfEnemies--;
+    }
+
+    public void IncreaseScore(int increase)
+    {
+        score += increase;
+        //scoreText.text = "Score: " + score;
+    }
+
+    public void DecreaseLives(int decrease)
+    {
+        lives -= decrease;
+        //livesText.text = "Lives: " + lives;
+    }
+}
---
+++ Assets/_Scripts/MovesAwayFromPlayer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovesAwayFromPlayer : MonoBehaviour {
+
+    private Transform player;
+    public float speed = -2f;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = GameObject.Find("Octo1").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 delta = player.position - transform.position;
+        delta.Normalize();
+        float moveSpeed = speed * Time.deltaTime;
+        transform.position = transform.position + (delta * moveSpeed);
+    }
+}
+
