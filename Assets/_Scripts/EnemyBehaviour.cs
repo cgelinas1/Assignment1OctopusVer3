75033e309d6d5c4e2d960999eb395251eba997d1
cgelinas1 <cgelinas@my.centennialcollege.ca>
2016-10-02 20:42:41 -0400

    Added Background Horizontal Control, Some Enemy Behaviour

---
+++ Assets/_Scripts/EnemyBehaviour.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+    public int health = 2;
+
+    // When the enemy dies, we play an explosion
+    public Transform explosion;
+
+    // sound to play when hit
+    //public AudioClip hitSound;
+
+    private GameController controller;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+        controller =
+            GameObject
+                .FindGameObjectWithTag("GameController")
+                .GetComponent<GameController>();
+    }
+
+    void OnCollisionEnter2D(Collision2D theCollision)
+    {
+        // Uncomment this line to check for collision
+        //Debug.Log("Hit"+ theCollision.gameObject.name);
+        // this line looks for "laser" in the names of
+        // anything collided.
+        if (theCollision.gameObject.name.Contains("Ink"))
+        {
+            InkBehaviour Ink =
+                theCollision.gameObject.GetComponent
+                ("LaserBehaviour") as InkBehaviour;
+            health -= Ink.damage;
+            Destroy(theCollision.gameObject);
+
+           
+        }
+
+        if (theCollision.gameObject.name.Contains("Octo1"))
+        {
+            controller.DecreaseLives(1);
+        }
+
+        if (health <= 0)
+        {
+            // Check if explosion was set
+            if (explosion)
+            {
+                GameObject exploder = ((Transform)Instantiate(explosion, this.
+                    transform.position, this.transform.rotation)).gameObject;
+                Destroy(exploder, 2.0f);
+            }
+
+            controller.KilledEnemy();
+            controller.IncreaseScore(10);
+
+
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/_Scripts/OceanController.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OceanController : MonoBehaviour {
+
+    private int _speed;
+    private Transform _transform;
+
+    // PUBLIC PROPERTIES
+    public int Speed
+    {
+        get
+        {
+            return this._speed;
+        }
+        set
+        {
+            this._speed = value;
+        }
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
+        this._transform = this.GetComponent<Transform>();
+
+        this._reset();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this._move();
+        this._checkBounds();
+    }
+
+    /**
+	 * this method moves the game object down the screen by _speed px every frame
+	 */
+    private void _move()
+    {
+        Vector2 newPosition = this._transform.position;
+
+        newPosition.x -= this._speed;
+
+        this._transform.position = newPosition;
+    }
+
+    /**
+	 * checks to see if the background meets the top-border of the screen
+	 */
+    private void _checkBounds()
+    {
+        if (this._transform.position.x <= -290f)
+        {
+            this._reset();
+        }
+    }
+
+    /**
+	 * resets game object to the original position
+	 */
+    private void _reset()
+    {
+        this._speed = 1;
+        this._transform.position = new Vector2(0f, 0f);
+    }
+}
